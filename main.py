62af0ee7f57e6b579096b7500aa50f1bc0c57b03
wayne <waynee.weeks@gmail.com>
2026-05-15 02:57:53 -0400

    intergrated delta time delay for 60 FPS

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock()
+    
+    clock = pygame.time.Clock()
     dt = 0
 
     while True:
@@ -20,6 +21,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()   
+    
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
